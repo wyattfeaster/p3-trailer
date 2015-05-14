86c76ad578b8bb2d81f2a3e982b5305952831818
Feaster <wfeaster@engr-134-11.eastern.ewu.edu>
2015-05-14 09:33:34 -0700

    Changed everything

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
 // MY SCRIPTS
 
-// var year = new Date.getYear();
-document.write("Hello World!");
+// Media Element Player
+
+$('audio,video').mediaelementplayer({
+	success: function(player, node) {
+	  $('#' + node.id + '-mode').html('mode: ' + player.pluginType);
+	},
+	startLanguage: 'en',
+	translationSelector: true
+});
 
 
+$('#video-filter').click(function(){
+
+  // hide audio
+  $('.mejs-audio').hide();
+  // show video
+  $('.mejs-video').show().addclass('pulse');
+  
+});
+
+$('#audio-filter').click(function(){
+  
+  // hide video
+  $('.mejs-video').hide();
+  // show video
+  $('.mejs-audio').show().addclass('pulse');
+  
+});
