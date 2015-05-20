0bf508b2c39e0c14d543208bfa2552d737ac6c88
wyattfeaster <wyattfeaster@wyatts-mbp.home>
2015-05-20 16:46:33 -0700

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,3 @@
-// MY SCRIPTS
+// My Scripts
 
-// Media Element Player
-
-$('audio,video').mediaelementplayer({
-	success: function(player, node) {
-	  $('#' + node.id + '-mode').html('mode: ' + player.pluginType);
-	},
-	startLanguage: 'en',
-	translationSelector: true
-});
-
-
-$('#video-filter').click(function(){
-
-  // hide audio
-  $('.mejs-audio').hide();
-  // show video
-  $('.mejs-video').show().addclass('pulse');
-  
-});
-
-$('#audio-filter').click(function(){
-  
-  // hide video
-  $('.mejs-video').hide();
-  // show video
-  $('.mejs-audio').show().addclass('pulse');
-  
-});
+$('video').mediaelementplayer();
